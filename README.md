84afe403d15c9d6a68caf69a7c66686bf655e728
qa-exercises <118732445+qa-exercises@users.noreply.github.com>
2022-12-22 01:32:43 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-exercise-render-a-list-from-data
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-exercise-render-a-list-from-data)
---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import ItemCard from './components/ItemCard'
+import './style.css'
+
+/*
+
+💪 Exercise: Render a list from data
+
+Here's our fruit market app - but where are the fruits? We want to see all the different items on sale, but this time we are going to achieve this by rendering a list of <ItemCard> components from the data found in src/itemsData.json.
+
+🎯 Your task:
+  - Import the fruit market items data from itemsData.json.
+  - Render a list of <ItemCard> components from this data.
+
+💡 Tips:
+  - Use the array map() function to transform the array of data into an array of components.
+  - Make sure each <ItemCard> receives a "symbol" prop, a "name" prop and a "price" prop.
+  - Don't forget to give each <ItemCard> a unique "key" prop. What uniquely identifies each item in the dataset?
+
+*/
+
+export default function App() {
+  return (
+    <main>
+      <h1>React Fruit Market</h1>
+      <div className="items-grid">
+        {/* --------- The list of ItemCard components goes here! --------- */}
+      </div>
+    </main>
+  )
+}
